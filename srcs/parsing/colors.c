283660f7bff7b6e493da80486e3eff86e6070c65
MiloCalise <milotavares6@gmail.com>
2026-02-14 13:19:33 +0100

    added executable for arch systems

--- srcs/parsing/colors.c
+++ srcs/parsing/colors.c
@@ -6,7 +6,7 @@
 /*   By: miltavar <miltavar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 14:02:12 by miltavar          #+#    #+#             */
-/*   Updated: 2026/02/14 12:36:39 by miltavar         ###   ########.fr       */
+/*   Updated: 2026/02/14 13:16:48 by miltavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static int	check_rgb(char *s)
 	return (0);
 }
 
-static int check_overflow_itn(int res, int sign, char digit)
+static int	check_overflow_itn(int res, int sign, char digit)
 {
 	if (res > (INT_MAX / 10))
 		return (1);
@@ -42,53 +42,53 @@ static int check_overflow_itn(int res, int sign, char digit)
 	return (0);
 }
 
-static int ft_atoi_ow(char *str, int *out)
+static int	ft_atoi_ow(char *str, int *out)
 {
-    int i = 0;
-    int sign = 1;
-    int res = 0;
+	int	i;
+	int	sign;
+	int	res;
 
-    if (str[i] == '-')
-        sign = -1, i++;
-
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        if (check_overflow_itn(res, sign, str[i]))
-            return (1);
-        res = res * 10 + (str[i] - '0');
-        i++;
-    }
-    *out = res * sign;
-    return (0);
+	1 && (i = 0, sign = 1, res = 0);
+	if (str[i] == '-')
+		1 && (sign = -1, i++);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (check_overflow_itn(res, sign, str[i]))
+			return (1);
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	*out = res * sign;
+	return (0);
 }
 
-static int parse_rgb_value(char *str, int *start, int *tab, int index)
+static int	parse_rgb_value(char *str, int *start, int *tab, int index)
 {
-    char *s;
-    int len = *start;
-    int i;
-    int tmp;
+	char	*s;
+	int		len;
+	int		i;
+	int		tmp;
 
-    while (str[*start] && ft_isdigit(str[*start]))
-        (*start)++;
-    i = *start;
-    skip_whitespaces(str, &i);
-    if (str[i] && str[i] != ',')
-        return (get_next_line(-42), 1);
-    s = ft_substr(str, len, *start - len);
-    if (!s)
-        return (get_next_line(-42), 1);
-    if (!*s || check_rgb(s))
-        return (free(s), get_next_line(-42), 1);
-    if (ft_atoi_ow(s, &tmp))
-        return (free(s), get_next_line(-42), 1);
-    if (tmp < 0 || tmp > 255)
-        return (free(s), get_next_line(-42), 1);
-    tab[index] = tmp;
-    skip_whitespaces(str, start);
-    return (free(s), (*start)++, 0);
+	len = *start;
+	while (str[*start] && ft_isdigit(str[*start]))
+		(*start)++;
+	i = *start;
+	skip_whitespaces(str, &i);
+	if (str[i] && str[i] != ',')
+		return (get_next_line(-42), 1);
+	s = ft_substr(str, len, *start - len);
+	if (!s)
+		return (get_next_line(-42), 1);
+	if (!*s || check_rgb(s))
+		return (free(s), get_next_line(-42), 1);
+	if (ft_atoi_ow(s, &tmp))
+		return (free(s), get_next_line(-42), 1);
+	if (tmp < 0 || tmp > 255)
+		return (free(s), get_next_line(-42), 1);
+	tab[index] = tmp;
+	skip_whitespaces(str, start);
+	return (free(s), (*start)++, 0);
 }
-
 
 int	grab_colors(int tab[3], char *str, char *tofind)
 {
